e6892a07f9d254ea9484ff6dca7b73311f389e89
陈相剑 <xiangjian.chen@neoantigen.cn>
2024-01-10 17:35:43 +0800

    功能和逻辑分离，测试

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,22 @@
 use std::env;
-use std::fs;
+use tsvlook::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     dbg!(&args);
 
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        std::process::exit(1);
+    });
 
     print!("Searching for {} ", config.query);
     println!("In file {}", config.file_path);
 
-    let contents = fs::read_to_string(config.file_path)
-        .expect("Shoudl be able to read the file");
-
-    println!("With text:\n{}", contents)
-}
-
-struct Config{
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Config {
-        if args.len() < 3 {
-            panic!("参数数量不足，应有2个参数")
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Config {query, file_path}
+    if let Err(e) = tsvlook::run(config) {
+        println!("Application error: {}", e);
+        std::process::exit(1);
     }
 }
+
+
